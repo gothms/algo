d3d651955b2033f1c0a2d588724a9fdef3e01106
gothms <gothmslee@gmail.com>
2023-06-01 15:41:43 +0800

    modify binary

--- binary/binarySearch.go
+++ binary/binarySearch.go
@@ -58,19 +58,30 @@ func BinarySearchFirstBigger(arr []int, t int) int {
 			r = mid
 		}
 	}
-	return l // 数组为空时，l 越界
+	return l // 数组为空时，l=0 越界
 }
 
 // BinarySearchLastSmaller 查找 最后一个小于等于 给定值的元素
 func BinarySearchLastSmaller(arr []int, t int) int {
-	l, r, mid := 0, len(arr), 0
-	for l < r {
-		mid = int(uint(l+r) >> 1)
+	l, r, mid := -1, len(arr)-1, 0
+	for l < r { // 拦住了 r=-1
+		mid = int(uint(l+r+1) >> 1)
 		if arr[mid] <= t {
-			l = mid + 1
+			l = mid
 		} else {
-			r = mid
+			r = mid - 1
 		}
 	}
-	return r - 1
+	return l // 错误：t<arr[0] 时返回了 0，则 l 初始化为 -1
+
+	//l, r, mid := 0, len(arr), 0
+	//for l < r {
+	//	mid = int(uint(l+r) >> 1)
+	//	if arr[mid] <= t {
+	//		l = mid + 1
+	//	} else {
+	//		r = mid
+	//	}
+	//}
+	//return r - 1
 }
