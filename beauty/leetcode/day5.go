0c52bc9a81f7bc578e6237bc3dcbe20049ae1f02
gothms <gothmslee@gmail.com>
2023-07-15 22:36:33 +0800

    new start

---
+++ beauty/leetcode/day5.go
@@ -0,0 +1,22 @@
+package leetcode
+
+/*
+二叉树
+	实现一个二叉查找树，并且支持插入、删除、查找操作
+	实现查找二叉查找树中某个节点的后继、前驱节点
+	实现二叉树前、中、后序以及按层遍历
+堆
+	实现一个小顶堆、大顶堆、优先级队列
+	实现堆排序
+	利用优先级队列合并 K 个有序数组
+	求一组动态数据集合的最大 Top K
+
+翻转二叉树
+	https://leetcode-cn.com/problems/invert-binary-tree/
+二叉树的最大深度
+	https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+验证二叉查找树
+	https://leetcode-cn.com/problems/validate-binary-search-tree/
+路径总和
+	https://leetcode-cn.com/problems/path-sum/
+*/
